bbfbcc59d5bca12d9abf43e708bde20d0bed838d
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2023-07-30 16:33:42 +0400

    Created_PassengerGenerator_Class_And_Methods

---
+++ src/main/java/PassengerGenerator.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.TimeUnit;
+
+public class PassengerGenerator extends Thread {
+    private Building building;
+    private int numFloors;
+    private int floor;
+
+    public PassengerGenerator(Building building, int numFloors, int floor) {
+        this.building = building;
+        this.numFloors = numFloors;
+        this.floor = floor;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(3); // New passenger time
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            int numPassengers = (int) (Math.random() * 5) + 1; // Random number of passengers from 1 to 5
+            System.out.println(numPassengers + " passenger " + floor + ". called the elevator on the floor.");
+            building.callElevator(floor);
+        }
+    }
+}
