6af15d2ba4c428791405e7b6f1b986297a55f9ac
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2023-07-30 16:26:12 +0400

    Created_Elevator_Class_And_Methods

---
+++ src/main/java/Elevator.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Elevator extends Thread {
+    private Building building;
+    private int numFloors;
+    private int currentFloor;
+    private boolean isMovingUp;
+    private Lock lock;
+
+    public Elevator(Building building, int numFloors) {
+        this.building = building;
+        this.numFloors = numFloors;
+        currentFloor = 0;
+        isMovingUp = true;
+        lock = new ReentrantLock();
+    }
+
+    public void run() {
+        while (true) {
+            // Changing the direction of the elevator
+            if (currentFloor == numFloors - 1)
+                isMovingUp = false;
+            else if (currentFloor == 0)
+                isMovingUp = true;
+
+            // Locking operation while the lift is moving
+            lock.lock();
+            try {
+                int numPassengers = building.getWaitingCount(currentFloor);
+                if (numPassengers > 0) {
+                    System.out.println("Asansör " + getId() + " katta " + numPassengers + " insanı aldı.");
+                    building.getInElevator(currentFloor);
+                }
+            } finally {
+                lock.unlock();
+            }
+
+            // Elevator moving to the next floor
+            try {
+                TimeUnit.SECONDS.sleep(4); // Elevator speed
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (isMovingUp)
+                currentFloor++;
+            else
+                currentFloor--;
+        }
+    }
+}
